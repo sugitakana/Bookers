3b8beb973045b163fc9f7f7358775ef927118382
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-10 16:16:16 +0000

    レイアウトの途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -21,9 +24,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  
-  validates :title, presence :ture
-  validates :body, presence :ture
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
