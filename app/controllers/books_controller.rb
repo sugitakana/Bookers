6504663f1ad84c0f2130e7f417740b6bb893c2cf
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-12 01:58:00 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :edit
     end
